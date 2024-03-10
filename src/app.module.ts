972f0d1f171b3d0fb397a00abaedeb5edaf03467
Oleh H <oleh.husiev.dev@gmail.com>
2024-03-10 02:37:57 +0100

    Move database config to sepporate module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -11,36 +10,12 @@ import { AccountModule } from './account/account.module';
 import { ImageModule } from './image/image.module';
 import { CoinService } from '~/coin/coin.service';
 import { CoinModule } from '~/coin/coin.module';
+import { DatabaseModule } from '~/database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mongodb',
-        autoLoadEntities: true,
-        url:
-          configService.get('NODE_ENV') === 'production'
-            ? `mongodb://${configService.get(
-                'MONGO_INITDB_ROOT_USERNAME',
-              )}:${configService.get(
-                'MONGO_INITDB_ROOT_PASSWORD',
-              )}@mongodb:${configService.get(
-                'MONGO_DB_PORT',
-              )}/${configService.get('MONGO_INITDB_DATABASE')}?authSource=admin`
-            : `mongodb://${configService.get(
-                'MONGO_INITDB_ROOT_USERNAME',
-              )}:${configService.get(
-                'MONGO_INITDB_ROOT_PASSWORD',
-              )}@${configService.get('MONGO_DB_URL')}:${configService.get(
-                'MONGO_DB_PORT',
-              )}/${configService.get(
-                'MONGO_INITDB_DATABASE',
-              )}?authSource=admin`,
-      }),
-    }),
+    DatabaseModule,
     ArchiveModule,
     AccountModule,
     ImageModule,
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,37 @@
 import { Module } from '@nestjs/common';
 import { ConnectionService } from './connection/connection.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mongodb',
+        autoLoadEntities: true,
+        url:
+          configService.get('NODE_ENV') === 'production'
+            ? `mongodb://${configService.get(
+                'MONGO_INITDB_ROOT_USERNAME',
+              )}:${configService.get(
+                'MONGO_INITDB_ROOT_PASSWORD',
+              )}@mongodb:${configService.get(
+                'MONGO_DB_PORT',
+              )}/${configService.get('MONGO_INITDB_DATABASE')}?authSource=admin`
+            : `mongodb://${configService.get(
+                'MONGO_INITDB_ROOT_USERNAME',
+              )}:${configService.get(
+                'MONGO_INITDB_ROOT_PASSWORD',
+              )}@${configService.get('MONGO_DB_URL')}:${configService.get(
+                'MONGO_DB_PORT',
+              )}/${configService.get(
+                'MONGO_INITDB_DATABASE',
+              )}?authSource=admin`,
+      }),
+    }),
+  ],
   providers: [ConnectionService],
 })
 export class DatabaseModule {}
