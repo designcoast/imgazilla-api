3f3afa6b3ce07a0822af61dd64312211f464d907
Oleh H <oleh.husiev.dev@gmail.com>
2024-03-12 21:59:03 +0100

    Add account service and controller

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Param, Query } from '@nestjs/common';
 
 @Controller('account')
 export class AccountController {
   constructor() {}
   @Get('check')
-  async check(): Promise<any> {
+  async check(
+    @Query('id') id: string,
+    @Query('name') name: string,
+  ): Promise<any> {
+    console.log('ID: ', id);
+    console.log('Name: ', name);
+
     return 'check user account';
   }
   @Post('create')
--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccountEntity } from './entities/account.entity';
 import { AccountController } from './account.controller';
+import { AccountService } from './account.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AccountEntity])],
   controllers: [AccountController],
+  providers: [AccountService],
 })
 export class AccountModule {}
