dce2aa673e9c23e9268266d03dab026493ba4683
Oleh H <oleh.husiev.dev@gmail.com>
2024-03-19 02:43:11 +0100

    Start add Shutdown service

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.use(helmet());
+  app.enableCors();
+
+  app.enableShutdownHooks();
 
   await app.listen(3000);
 }
