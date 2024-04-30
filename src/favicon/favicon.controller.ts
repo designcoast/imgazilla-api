9e22240daa6161abf81563773d507a42c5bfff5e
Oleh H <oleh.husiev.dev@gmail.com>
2024-04-30 23:55:42 +0200

    Update favicon controller

--- src/favicon/favicon.controller.ts
+++ src/favicon/favicon.controller.ts
@@ -1,15 +1,13 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Post,
-  Header,
   UploadedFile,
   UseInterceptors,
-  Res,
-  BadRequestException,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { Response } from 'express';
+
 import {
   GenerateFaviconOptionsDto,
   generateFaviconOptionsSchema,
@@ -22,25 +20,19 @@ export class FaviconController {
   constructor(private readonly faviconService: FaviconService) {}
 
   @Post('generate')
-  @Header('Content-Type', 'application/zip')
-  @Header('Content-Disposition', 'attachment; filename=imgazilla.zip')
   @UseInterceptors(FileInterceptor('image'))
   async generateFavicon(
     @UploadedFile() image: Express.Multer.File,
     @Body(new YupValidationPipe(generateFaviconOptionsSchema))
     options: GenerateFaviconOptionsDto,
-    @Res({ passthrough: true }) res: Response,
-  ): Promise<any> {
+  ): Promise<
+    {
+      name: string;
+      buffer: Uint8Array;
+    }[]
+  > {
     try {
-      const faviconImages = await this.faviconService.generateFavicon(
-        image.buffer,
-        options,
-      );
-
-      // console.log('faviconImages', faviconImages);
-      // return iconBuffer;
-      const buffer = await this.faviconService.createArchive(faviconImages);
-      res.send(buffer);
+      return await this.faviconService.generateFavicon(image.buffer, options);
     } catch (error) {
       throw new BadRequestException(error.message);
     }
