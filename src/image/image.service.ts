beb903f2cc7139fc7068f8c03fdda76e4277c810
Oleh H <oleh.husiev.dev@gmail.com>
2024-05-22 01:19:50 +0200

    Update image optimization service

--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -1,27 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import * as sharp from 'sharp';
-import { IImageOptimizationOptions } from '~/types';
-import { getImageOptimizationFnByFormat } from '~/image/image.utils';
+
+import { InjectQueue } from '@nestjs/bull';
+import { Queue } from 'bullmq';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class ImageService {
+  constructor(
+    @InjectQueue('image-processing') private readonly processingQueue: Queue,
+  ) {}
   async optimizeImage(data: any): Promise<any> {
-    console.log('optimizeImage', data);
-    // try {
-    //   const { quality, outputFormat } = options;
-    //
-    //   const imageProcessor = sharp(image.buffer).toFormat(outputFormat);
-    //
-    //   const optimizedImageBuffer = await getImageOptimizationFnByFormat(
-    //     outputFormat,
-    //   )(imageProcessor, quality).toBuffer();
-    //
-    //   //TODO: Add logging here
-    //   return optimizedImageBuffer;
-    // } catch (error) {
-    //   throw new Error('Failed to optimize image');
-    // }
+    const chunkId = uuid();
+    await this.processingQueue.add(chunkId, data);
   }
 }
-// image: Buffer,
-//   options: IImageOptimizationOptions,
