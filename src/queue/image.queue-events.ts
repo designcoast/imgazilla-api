b6e6277b8b383d2f3d048bef94839e5b99160223
Oleh H <oleh.husiev.dev@gmail.com>
2024-05-26 21:48:00 +0200

    Update queue event function

--- src/queue/image.queue-events.ts
+++ src/queue/image.queue-events.ts
@@ -10,11 +10,7 @@ export class ImageQueueEvents extends QueueEventsHost {
   private readonly logger = new Logger(ImageQueueEvents.name);
 
   @OnQueueEvent('completed')
-  onCompleted(jobId) {
-    this.logger.debug('Start completed event...');
-    // this.logger.debug(jobId);
-    console.log('jobId', jobId);
-    this.logger.debug('Finishing completed event');
-    return 'hi';
+  onCompleted(job: { jobId: string; returnvalue: unknown }) {
+    this.logger.log(`Job ${job.jobId} completed`);
   }
 }
