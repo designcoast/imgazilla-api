60d4425609b1487b611a618eb9e7f10039af459c
Oleh H <oleh.husiev.dev@gmail.com>
2024-05-26 21:47:47 +0200

    Update result of compression

--- src/queue/image.processor.ts
+++ src/queue/image.processor.ts
@@ -3,29 +3,45 @@ import { Logger } from '@nestjs/common';
 
 import { Job } from 'bullmq';
 import * as sharp from 'sharp';
-import { decode } from 'base64-arraybuffer';
+import { decode, encode } from 'base64-arraybuffer';
 
 import { getImageOptimizationFnByFormat } from '~/image/image.utils';
-import { ImageOptions } from '~/types';
 import { IMAGE_OPTIMISATION_FORMATS } from '~/constants';
+import { ImageOptimizationDto } from '~/image/dto/optimaze-image.dto';
 
 @Processor('image-processing')
 export class ImageQueueProcessor extends WorkerHost {
   private readonly logger = new Logger(ImageQueueProcessor.name);
 
   async process(job: Job) {
-    const result = await Promise.all(
-      job.data.map(async ({ uintArray, optimizationPercent }: ImageOptions) => {
-        const buffer = decode(uintArray);
-        const imageProcessor = sharp(buffer).toFormat(
-          IMAGE_OPTIMISATION_FORMATS.PNG,
-        );
+    try {
+      return await Promise.all(
+        job.data.map(
+          async ({
+            base64Image,
+            optimizationPercent,
+          }: ImageOptimizationDto) => {
+            const buffer = decode(base64Image);
+            const imageProcessor = sharp(buffer).toFormat(
+              IMAGE_OPTIMISATION_FORMATS.PNG,
+            );
 
-        return await getImageOptimizationFnByFormat(
-          IMAGE_OPTIMISATION_FORMATS.PNG,
-        )(imageProcessor, optimizationPercent).toBuffer();
-      }),
-    );
-    return result;
+            const imageBuffer = await getImageOptimizationFnByFormat(
+              IMAGE_OPTIMISATION_FORMATS.PNG,
+            )(imageProcessor, optimizationPercent).toBuffer();
+
+            const sizeInBytes = imageBuffer.length;
+            const sizeInKB = sizeInBytes / 1024;
+
+            return {
+              base64Image: encode(imageBuffer),
+              optimizedImageSize: sizeInKB,
+            };
+          },
+        ),
+      );
+    } catch (e) {
+      this.logger.error(e);
+    }
   }
 }
