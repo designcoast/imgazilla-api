95eee55d110aa25096dcc49ba2c13efc3e854e91
Oleh H <oleh.husiev.dev@gmail.com>
2024-05-26 21:47:35 +0200

    Add a new methods for image optimization controller

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,17 +1,38 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ImageService } from '~/image/image.service';
+import { YupValidationPipe } from '~/pipes/yup-validation.pipe';
+import {
+  ImageOptimizationDto,
+  imageOptimizationSchema,
+} from '~/image/dto/optimaze-image.dto';
+import { ImageOptimizationResult } from '~/types';
 
 @Controller('image')
 export class ImageController {
   constructor(private readonly imageService: ImageService) {}
 
   @Post('optimize')
-  // @UseInterceptors(FileInterceptor('image'))
   async optimizeImage(
-    // @UploadedFile() image: Express.Multer.File,
-    @Body() image: any,
-    // options: IImageOptimizationOptions,
-  ) {
-    await this.imageService.optimizeImage(image);
+    @Body(new YupValidationPipe(imageOptimizationSchema))
+    image: ImageOptimizationDto[],
+  ): Promise<string> {
+    return await this.imageService.optimizeImage(image);
+  }
+
+  @Get(':id/status')
+  async getImageStatus(@Param('id') id: string): Promise<{
+    status: number;
+    reason?: string;
+  }> {
+    return await this.imageService.getImageStatus(id);
+  }
+
+  @Get(':id/result')
+  async getImageOptimizationResult(@Param('id') id: string): Promise<{
+    status: number;
+    reason?: string;
+    result?: ImageOptimizationResult;
+  }> {
+    return await this.imageService.getImageOptimizationResult(id);
   }
 }
