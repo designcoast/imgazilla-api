f44e42756e77758b30cc513963815f85832c8ae9
Oleh H <oleh.husiev.dev@gmail.com>
2024-06-10 00:11:26 +0200

    Update headers guard

--- src/guards/headers.guard.ts
+++ src/guards/headers.guard.ts
@@ -9,7 +9,6 @@ import {
 import { Observable } from 'rxjs';
 
 import { ConfigService } from '@nestjs/config';
-// import { verifySignature } from '~/utils/verifySignature.utils';
 
 @Injectable()
 export class HeadersGuard implements CanActivate {
@@ -20,35 +19,13 @@ export class HeadersGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    // const request = context.switchToHttp().getRequest();
-    //
-    // const figmaHeader = request.headers['x-figma-signature'];
-    // console.log('figmaHeader', figmaHeader);
-    // const figmaRequestsSecret = this.configService.get('REQUESTS_SECRET_KEY');
-    // const isFigmaHeaderVerified = verifySignature(
-    //   request,
-    //   figmaHeader,
-    //   figmaRequestsSecret,
-    // );
-    // console.log('isFigmaHeaderVerified', isFigmaHeaderVerified);
-    // const signatureHeader = request.headers['X-Signature'];
-    //
-    // const hmac = crypto.createHmac(
-    //   'sha256',
-    //   this.configService.get('LEMONSQUEEZY_WEBHOOK_SECRET'),
-    // );
-    //
-    // const digest = Buffer.from(
-    //   hmac.update(request.rawBody).digest('hex'),
-    //   'utf8',
-    // );
-    // const signature = Buffer.from(signatureHeader || '', 'utf8');
-    //
-    // const isSignatureHeaderAllowed = crypto.timingSafeEqual(digest, signature);
+    const request = context.switchToHttp().getRequest();
+    const figmaHeader = request.headers['x-figma-signature'];
+    const signatureHeader = request.headers['s-signature'];
 
-    // if (!requiredHeader || requiredHeader !== 'expected-value') {
-    //   throw new ForbiddenException('Forbidden');
-    // }
+    if (!figmaHeader || signatureHeader) {
+      throw new ForbiddenException('Forbidden');
+    }
 
     return true;
   }
