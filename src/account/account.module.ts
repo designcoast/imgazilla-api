f3d0640581946fe7620178f7c3ab9b2f5f0fe9b7
Oleh H <oleh.husiev.dev@gmail.com>
2024-06-15 13:20:44 +0200

    Add export module field

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -8,5 +8,6 @@ import { AccountService } from './account.service';
   imports: [TypeOrmModule.forFeature([AccountEntity])],
   controllers: [AccountController],
   providers: [AccountService],
+  exports: [AccountService],
 })
 export class AccountModule {}
