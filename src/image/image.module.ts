b4b04fb48bf3b6cc11c0979e3f3e467e8bf9eccd
Oleh H <oleh.husiev.dev@gmail.com>
2024-06-18 01:15:41 +0200

    Add config and account imports

--- src/image/image.module.ts
+++ src/image/image.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
 import { ImageController } from './image.controller';
 import { ImageService } from './image.service';
 import { ImageQueueModule } from '~/queue/image-queue.module';
+import { AccountModule } from '~/account/account.module';
 
 @Module({
-  imports: [ImageQueueModule],
+  imports: [ImageQueueModule, AccountModule],
   controllers: [ImageController],
-  providers: [ImageService],
+  providers: [ImageService, ConfigService],
 })
 export class ImageModule {}
