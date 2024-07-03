cc1a8cf31dc76c1743ea7ad2f69e08c3a51a2798
Oleh H <oleh.husiev.dev@gmail.com>
2024-07-03 21:47:33 +0200

    Update database module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConnectionService } from './connection/connection.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmConfigService } from '~/configs/typeorm.config';
+import { TypeOrmConfigService } from '~/configs/typeOrmConfigService';
 
 @Module({
   imports: [
