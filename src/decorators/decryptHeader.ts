4c891fcf8f91ac6ef6d3be5f0fd7867258e549c9
Oleh H <oleh.husiev.dev@gmail.com>
2024-10-16 21:42:56 +0200

    Add condition for dev env for testing

--- src/decorators/decryptHeader.ts
+++ src/decorators/decryptHeader.ts
@@ -9,6 +9,8 @@ const secret = process.env.REQUESTS_SECRET_KEY;
 
 export const DecryptHeader = createParamDecorator(
   (data: string, ctx: ExecutionContext) => {
+    const isDevelopmentEnv = process.env.NODE_ENV === 'development';
+
     const request = ctx.switchToHttp().getRequest();
     const encryptedValue = request.headers['x-figma-id'];
 
@@ -17,6 +19,10 @@ export const DecryptHeader = createParamDecorator(
     }
 
     try {
+      if (isDevelopmentEnv) {
+        return encryptedValue; // for development, return raw value for easier testing
+      }
+
       return decrypt(encryptedValue, secret);
     } catch (error) {
       throw new Error('Invalid encrypted value');
