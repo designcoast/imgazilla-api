33523564f98ea56c381eb6c906bf23ba4fff3e18
Oleh H <oleh.husiev.dev@gmail.com>
2024-12-05 22:59:15 +0100

    Add migration for a new column

---
+++ src/database/migrations/1733435258120-migrations.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Migrations1733435258120 implements MigrationInterface {
+  name = 'Migrations1733435258120';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "Account" ADD "has_bonus" boolean NOT NULL DEFAULT false`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "Account" DROP COLUMN "has_bonus"`);
+  }
+}
