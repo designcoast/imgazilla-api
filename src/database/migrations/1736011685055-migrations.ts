4a2e4201756876eeb816381d89dc92f74e140441
Oleh H <oleh.husiev.dev@gmail.com>
2025-01-04 19:33:00 +0100

    Update migrations

--- src/database/migrations/1736011685055-migrations.ts
+++ src/database/migrations/1736011685055-migrations.ts
@@ -1,25 +1,25 @@
-// import { MigrationInterface, QueryRunner, Table } from 'typeorm';
-//
-// export class Migrations1736011685055 implements MigrationInterface {
-//   public async up(queryRunner: QueryRunner): Promise<void> {
-//     await queryRunner.createTable(
-//       new Table({
-//         name: 'Account',
-//         columns: [
-//           { name: 'id', type: 'serial', isPrimary: true },
-//           { name: 'name', type: 'varchar', isNullable: true },
-//           { name: 'photoUrl', type: 'varchar', isNullable: true },
-//           { name: 'figmaUserID', type: 'varchar', isNullable: false },
-//           { name: 'credits', type: 'int', default: '30', isNullable: false },
-//           { name: 'has_bonus', type: 'boolean', default: false },
-//           { name: 'createAt', type: 'timestamp', default: 'CURRENT_TIMESTAMP' },
-//           { name: 'updateAt', type: 'timestamp', default: 'CURRENT_TIMESTAMP' },
-//         ],
-//       }),
-//     );
-//   }
-//
-//   public async down(queryRunner: QueryRunner): Promise<void> {
-//     await queryRunner.dropTable('Account');
-//   }
-// }
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class Migrations1736011685055 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'Account',
+        columns: [
+          { name: 'id', type: 'serial', isPrimary: true },
+          { name: 'name', type: 'varchar', isNullable: true },
+          { name: 'photoUrl', type: 'varchar', isNullable: true },
+          { name: 'figmaUserID', type: 'varchar', isNullable: false },
+          { name: 'credits', type: 'int', default: '30', isNullable: false },
+          { name: 'has_bonus', type: 'boolean', default: false },
+          { name: 'createAt', type: 'timestamp', default: 'CURRENT_TIMESTAMP' },
+          { name: 'updateAt', type: 'timestamp', default: 'CURRENT_TIMESTAMP' },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('Account');
+  }
+}
