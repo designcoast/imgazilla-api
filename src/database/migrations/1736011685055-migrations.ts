13ded160e4037750f575383195d0b5fc2d213440
Oleh H <oleh.husiev.dev@gmail.com>
2025-01-04 18:36:00 +0100

    Update migration

--- src/database/migrations/1736011685055-migrations.ts
+++ src/database/migrations/1736011685055-migrations.ts
@@ -9,7 +9,7 @@ export class Migrations1736011685055 implements MigrationInterface {
           { name: 'id', type: 'serial', isPrimary: true },
           { name: 'name', type: 'varchar', isNullable: true },
           { name: 'photoUrl', type: 'varchar', isNullable: true },
-          { name: 'figma_user_id', type: 'varchar', isNullable: false },
+          { name: 'figmaUserID', type: 'varchar', isNullable: false },
           { name: 'credits', type: 'int', default: '30', isNullable: false },
           { name: 'has_bonus', type: 'boolean', default: false },
           { name: 'createAt', type: 'timestamp', default: 'CURRENT_TIMESTAMP' },
